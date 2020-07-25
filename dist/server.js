51fc83f8dfd2fd7b461d2d9412019d070cd03f3d
MinasXirakias <minasxirak@gmail.com>
2020-07-25 14:51:44 +0300

    fav icon and external style file

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("*", (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "/") });
 });
