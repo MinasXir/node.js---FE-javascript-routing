4a16bafd6959bc6e6930dce2a210c1a21f933e0b
MinasXirakias <minasxirak@gmail.com>
2020-07-25 19:40:58 +0300

    dark mode

--- dist/server.js
+++ dist/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3000;
-app.use(express.static(__dirname + "/style.css"));
 
 app.get("*", (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "/") });
