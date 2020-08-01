ac7c3e731e1114b5d805ed837c34e65d84601b73
MinasXirakias <minasxirak@gmail.com>
2020-08-01 18:54:01 +0300

    add routes

--- dist/server.js
+++ dist/server.js
@@ -7,6 +7,12 @@ app.use(express.static(__dirname + "/public"));
 app.get("*", (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "/") });
 });
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "/about") });
+});
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "/contact") });
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
