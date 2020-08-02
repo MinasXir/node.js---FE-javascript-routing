461d0bb75cc42deb7d137a134fbffe0007fa21e3
MinasXirakias <minasxirak@gmail.com>
2020-08-02 23:28:27 +0300

    working wit

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const app = express();
+let app = express();
 const path = require("path");
 const port = 3000;
+
 app.use(express.static(__dirname + "/public"));
 
 app.get("*", (req, res) => {
