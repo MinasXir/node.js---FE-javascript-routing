0e8f63fc166c6204ea59eac641c2e5a248abee89
MinasXirakias <minasxirak@gmail.com>
2020-08-15 13:37:56 +0300

    testing routes

--- dist/server.js
+++ dist/server.js
@@ -5,13 +5,7 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/mywebsite/", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "index.html"));
-});
-app.get("/about", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "index.html"));
-});
-app.get("/contact", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
