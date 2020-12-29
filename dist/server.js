9898bab581a2df09463955e109f0dde4696eb128
MinasXirakias <minasxirak@gmail.com>
2020-12-29 22:26:05 +0200

    adding styles path2

--- dist/server.js
+++ dist/server.js
@@ -5,9 +5,11 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static('./public/images'));
-
-app.get("*", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve("index.html"));
+});
+app.get("/*", function (req, res) {
+  res.sendFile(path.resolve("index.html"));
 });
 
 app.listen(port, () =>
